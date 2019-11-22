59128fd85b32c6e34ca1aebc5018a9eba2f65e3e
D. Henkes <hello@henkes.io>
2019-11-22 10:43:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -21,3 +21,30 @@ exports.get = function (obj, path, keys = []) {
 
   return this.get(obj[keys[0]], path.substring(keys[0].length + 1), keys.slice(1));
 }
+
+exports.set = function (obj, path, value) {
+  if (!obj) {
+    obj = {};
+  }
+
+  if (typeof path !== 'string' || path.length === 0) {
+    return obj;
+  }
+
+  const index = path.indexOf('.');
+  const key = (index > -1 ? path.substring(0, index) : path);
+
+  if (typeof obj[key] === 'undefined') {
+    if (Number.isInteger(Number(key)) && Array.isArray(obj) === false) {
+      obj = [];
+    }
+  }
+
+  if (index === -1) {
+    obj[key] = value;
+    return obj;
+  }
+
+  obj[key] = this.set(obj[key], path.substring(key.length + 1), value);
+  return obj;
+}
